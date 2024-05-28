34c151ed47b7992977cb4be246af75605346e2a0
saimsohailKH <saimsohil466@gmail.com>
2024-05-28 22:40:09 +0500

    Project document

---
+++ document.rb
@@ -0,0 +1,32 @@
+# cst requirements
+# add to cat
+# items 
+# oder
+
+#admin requirements
+# add new listing 
+# can check oder / update as well 
+# cst list /  data
+
+#classes
+#user
+#item 
+#oder
+#oder_item
+#cst
+#admin
+
+#Attributrs of user class 
+#name , id , email, ph no, address
+
+#attributes of item
+#name, id, price
+
+#oder_item 
+#item_id, id, quantity, total 
+
+#aTRRIBUTES of oder
+#id, cst_id, total amount,oder item 
+
+# attributes of customer
+# orders
